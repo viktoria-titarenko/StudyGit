49e0bfc1065f26ac437e5d38f9fdaa450f2f5de2
Pchelnikov_VN <Pchelnikov_VN@directum.ru>
2022-02-15 11:44:31 +0400

    создан проект

---
+++ DirRX.Solution/DirRX.Solution.Server/ModuleJobs.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+
+namespace DirRX.Solution.Server
+{
+  public class ModuleJobs
+  {
+
+  }
+}
---
+++ DirRX.Solution/DirRX.Solution.Shared/ModuleConstants.cs
@@ -0,0 +1,10 @@
+using System;
+using Sungero.Core;
+
+namespace DirRX.Solution.Constants
+{
+  public static class Module
+  {
+
+  }
+}
